58f1a7e8efc35efb6394ab7effba286624f75db8
Pranav Sinha <pranavsinha922@gmail.com>
2023-07-15 15:04:35 +0530

    Add files via upload

---
+++ MoveZeroes.cpp
@@ -0,0 +1,23 @@
+// 283. Move Zeroes
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j  = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] != 0) {
+                nums[j++] = nums[i];
+            }
+        }
+        for (; j < nums.size(); j++) 
+            nums[j] = 0;
+    }
+};
+
+signed main(void) {
+    
+}
