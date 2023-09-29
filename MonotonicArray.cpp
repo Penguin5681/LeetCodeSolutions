2b2fac333c8dfee1808d4d779cae00db4a5df049
Pranav Sinha <pranavsinha922@gmail.com>
2023-09-29 10:45:27 +0530

    Add files via upload

---
+++ MonotonicArray.cpp
@@ -0,0 +1,28 @@
+// 896. Monotonic Array
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        bool increasing = true;
+        bool decreasing = true;
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] > nums[i - 1])
+                decreasing = false;
+            else if (nums[i] < nums[i - 1])
+                increasing = false;
+        }
+
+        if (!decreasing and !increasing)
+            return false;
+        return true;
+    }
+};
+
+signed main() {
+
+}
