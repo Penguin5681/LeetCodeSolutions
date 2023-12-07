af9f2b6a5beb4a6161793afb75c234e548724881
Pranav Sinha <pranavsinha922@gmail.com>
2023-12-07 08:30:44 +0530

    Add files via upload

---
+++ LargestOddNumberinString.cpp
@@ -0,0 +1,21 @@
+// 1903. Largest Odd Number in String
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        for (int i = num.size() - 1; i >= 0; --i) {
+            if ((num[i] - '0') % 2 == 1) {
+                return num.substr(0, i + 1);
+            }
+        }
+        return "";
+    }
+};
+
+signed main() {
+
+}
